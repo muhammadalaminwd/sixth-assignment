c10a4e83ea9bd40b0c84524efc632413ddc88fb9
Muhammad Al Amin <muhammadalaminwd@gmail.com>
2022-03-01 19:46:06 +0600

    phone detail modified

--- js/app.js
+++ js/app.js
@@ -36,10 +36,10 @@ const displayPhone = (data) => {
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
             <h3 class="card-title">${phone.phone_name}</h3>
-            <h5>Category: ${phone.brand}</h5>
-            <button  onclick="seeDetails(${phone.mainFeatures})" class="btn btn-primary">Phone Details</button>
+            <h5>Brand: ${phone.brand}</h5>
+            <button  onclick="phoneDetails('${phone.slug}')" class="btn btn-primary w-100">Phone Details</button>
             </div>
-        </div>
+        </div>phop
             `;
             phoneContainer.appendChild(div);
         });
@@ -48,22 +48,28 @@ const displayPhone = (data) => {
 
 //  Get the phone Details
 const phoneDetails = phoneId => {
-    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId.slug}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => displayDetails(data.mainFeatures))
+    .then(info => displayDetails(info.data));
 }
 
 // Display the phone Details
-const displayDetails = singleItem =>{
-    phoneContainer.innerHTML = `
+const displayDetails = singlePhone =>{
+    console.log(singlePhone)
+    const phoneDetail = document.getElementById('phone-detail');
+    const div = document.createElement('div')
+    div.innerHTML = `
     <div class="card" style="width: 18rem;">
-    <img src="${singleItem.image}" class="card-img-top" alt="...">
+    <img src="${singlePhone.image}" class="card-img-top" alt="...">
     <div class="card-body">
-        <h3 class="card-title">${singleItem.phone_name}</h3>
-        <h5>Category: ${singleItem.brand}</h5>
-        <p>${singleItem.mainFeatures}</p>
+        <h3 class="card-title">${singlePhone.phone_name}</h3>
+        <h5>Brand: ${singlePhone.brand}</h5>
+        <p>${singlePhone.mainFeatures}</p>
+        <a href="${singlePhone.others}" class="btn btn-primary">Go somewhere</a>
+        </div>
     </div>
-  </div>
-    `
+    `;
+    phoneDetail.appendChild(div);
 }
+
