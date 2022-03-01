e24508e2b3594199f6c3af3fa83aa5ee44359285
Muhammad Al Amin <muhammadalaminwd@gmail.com>
2022-03-01 10:32:27 +0600

    API call

--- js/app.js
+++ js/app.js
@@ -3,3 +3,18 @@ const inputPhone = document.getElementById('input-phone');
 const searchButton = document.getElementById('search-button');
 const phoneContainer = document.getElementById('phone-container');
 
+const searchPhone = () =>{
+    const inputValue = inputPhone.value;
+    if(inputValue.length == ''){
+        phoneContainer.innerHTML = `
+        <h1 class="text-center my-4 text-warning">Please write a phone name!</h1>
+        `;
+    }
+    else{
+        const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        inputPhone.value = '';
+    }
+};
