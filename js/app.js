cc8cef34bcbf37b73ab3433148c43f9fe49f8eae
Muhammad Al Amin <muhammadalaminwd@gmail.com>
2022-03-01 10:00:07 +0600

    got the variables

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Get all variables
+const inputPhone = document.getElementById('input-phone');
+const searchButton = document.getElementById('search-button');
+const phoneContainer = document.getElementById('phone-container');
+
