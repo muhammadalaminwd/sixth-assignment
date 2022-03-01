9be0374b793b14c9fcda73be1a13dea5c07a838d
Muhammad Al Amin <muhammadalaminwd@gmail.com>
2022-03-01 11:09:07 +0600

    displaying phones

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const inputPhone = document.getElementById('input-phone');
 const searchButton = document.getElementById('search-button');
 const phoneContainer = document.getElementById('phone-container');
 
+// Get the phone API
 const searchPhone = () =>{
     const inputValue = inputPhone.value;
     if(inputValue.length == ''){
@@ -14,7 +15,33 @@ const searchPhone = () =>{
         const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
         fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayPhone(data.data))
         inputPhone.value = '';
+    }
+};
+
+// Display the phone
+const displayPhone = (data) => {
+    if(!data){
+        phoneContainer.innerHTML = `
+        <h1 class="text-center my-4 text-warning">No result found</h1>
+        `;
+    }
+    else{
+        phoneContainer.innerHTML = "";
+        data?.forEach((phone) => {
+            const div = document.createElement("div");
+            div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h3 class="card-title">${phone.phone_name}</h3>
+            <h5>Category: ${phone.brand}</h5>
+            <button  onclick="seeDetails(${phone.others})" class="btn btn-primary">See Details</button>
+            </div>
+        </div>
+            `;
+            phoneContainer.appendChild(div);
+        });
     }
 };
